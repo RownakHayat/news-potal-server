0ea36dedd41ee9b6218b3589e1771bc70ee8b43d
unknown <rownakhayat@gmail.com>
2023-07-04 16:03:08 +0600

    change

--- index.js
+++ index.js
@@ -3,17 +3,42 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const category = require('./data/caregory.json')
+const category = require('./data/category.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('News')
 });
 
-app.get('/news-category', (req, res) =>{
+app.get('/news-category', (req, res) => {
     res.send(category);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '00') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(news => news.category_id === id);
+        res.send(categoryNews);
+    }
+});
+
+app.get('/news', (req, res) =>{
+    res.send(news);
 })
-app.listen(port, ()=>{
-    console.log("Nes protal", port);
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(news => news._id === id);
+    res.send(selectedNews);
+    console.log(req.params.id);
+});
+
+
+app.listen(port, () => {
+    console.log("News protal", port);
 });
