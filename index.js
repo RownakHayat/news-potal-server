425cc7860bd317f365880dec446108b538c1a27f
unknown <rownakhayat@gmail.com>
2023-07-10 18:20:07 +0600

    update

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
-const category = require('./data/category.json');
-const news = require('./data/news.json');
-
 app.use(cors());
 
+const category = require('./data/category.json');
+const news = require('./data/news.json')
+
+
 app.get('/', (req, res) => {
-    res.send('News')
+    res.send('News running')
 });
 
 app.get('/news-category', (req, res) => {
@@ -18,25 +18,28 @@ app.get('/news-category', (req, res) => {
 
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
-    if (id === '00') {
-        res.send(news);
+    if (id === '08') {
+        res.send(news)
     }
     else {
-        const categoryNews = news.filter(news => news.category_id === id);
-        res.send(categoryNews);
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
     }
-});
 
+})
 app.get('/news', (req, res) =>{
     res.send(news);
 })
 
 app.get('/news/:id', (req, res) => {
     const id = req.params.id;
-    const selectedNews = news.find(news => news._id === id);
+    const selectedNews = news.find(n => n._id === id);
     res.send(selectedNews);
-    console.log(req.params.id);
-});
+
+
+})
+
+
 
 
 app.listen(port, () => {
